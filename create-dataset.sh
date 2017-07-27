f9c81b26cc979da50feaa44cb8a7812dc6383cdf
Daker Fernandes Pinheiro <dfp@cin.ufpe.br>
2017-07-27 10:28:47 -0300

    update zip

--- create-dataset.sh
+++ create-dataset.sh
@@ -29,10 +29,38 @@ function render_multires()
 	rm "./$1"
 }
 
+function augment_multires()
+{
+	DIRNAME="./dataset/$1"
+	ZIPFILE="./dataset/$1.zip"
+	go build -o "./$1" "./examples/$1.go"
+	"./$1" -w 32 -h 32 -interactions $2 -path "/tmp/1-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/1-%04d.npy" "1-%04d.npy" $2
+	"./$1" -w 60 -h 40 -interactions $2 -path "/tmp/2-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/2-%04d.npy" "2-%04d.npy" $2
+	"./$1" -w 120 -h 80 -interactions $2 -path "/tmp/3-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/3-%04d.npy" "3-%04d.npy" $2
+	"./$1" -w 240 -h 160 -interactions $2 -path "/tmp/4-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/4-%04d.npy" "4-%04d.npy" $2
+	"./$1" -w 640 -h 480 -interactions $2 -path "/tmp/5-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/5-%04d.npy" "5-%04d.npy" $2
+	"./$1" -w 1280 -h 960 -interactions $2 -path "/tmp/6-%04d.npy"
+	python ./update-zip.py "./dataset/$1.zip" "/tmp/6-%04d.npy" "6-%04d.npy" $2
+	rm -rf /tmp/*.npy
+}
+
+
+# for EXAMPLE in $EXAMPLES
+# do
+# 	render_multires $EXAMPLE 1024
+# done
+
 for EXAMPLE in $EXAMPLES
 do
-	render_multires $EXAMPLE 1024
+	augment_multires $EXAMPLE 32
 done
+
+
 
 # "./$1" -w 60 -h 40 -interactions $2 -path "dataset/$1-1-%04d.npy"
 # "./$1" -w 120 -h 80 -interactions $2 -path "dataset/$1-2-%04d.npy"
--- update-zip.py
+++ update-zip.py
@@ -1,6 +1,10 @@
-from sys import argv
+import sys
 import zipfile as zf
 
-z = zf.ZipFile(argv[1], mode='a')
-z.write(argv[2], argv[3])
+z = zf.ZipFile(sys.argv[1], mode='a', allowZip64=True)
+spp = int(sys.argv[4])
+filename = sys.argv[2] % spp
+to = sys.argv[3] % spp
+print(filename, to)
+z.write(filename, to)
 z.close()
